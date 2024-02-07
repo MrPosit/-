737fad2ed2dc9ee3ea1c867d617e81bdf50d1a0f
Еркин Адилжан Асхатулы <ErkinAd@lcoast.itstep>
2024-02-07 19:10:58 +0600

    first commit

--- hw14/main.py
+++ hw14/main.py
@@ -4,53 +4,53 @@
 год выпуска, производителя, объем двигателя, цвет машины, цену. Реализуйте методы класса 
 для ввода данных, вывода данных, реализуйте доступ к отдельным полям через методы класса.
 '''
-# class Car:
-#     def __init__(self, model, year, manufacturer, engine_volume, color, price):
-#         self.model = model
-#         self.year = year
-#         self.manufacturer = manufacturer
-#         self.engine_volume = engine_volume
-#         self.color = color
-#         self.price = price
+class Car:
+    def __init__(self, model, year, manufacturer, engine_volume, color, price):
+        self.model = model
+        self.year = year
+        self.manufacturer = manufacturer
+        self.engine_volume = engine_volume
+        self.color = color
+        self.price = price
 
-#     def input_data(self):
-#         self.model = input("Модель: ")
-#         self.year = input("Год: ")
-#         self.manufacturer = input("Производитель: ")
-#         self.engine_volume = input("Обьем двигателя: ")
-#         self.color = input("Цвет: ")
-#         self.price = input("Цена: ")
+    def input_data(self):
+        self.model = input("Модель: ")
+        self.year = input("Год: ")
+        self.manufacturer = input("Производитель: ")
+        self.engine_volume = input("Обьем двигателя: ")
+        self.color = input("Цвет: ")
+        self.price = input("Цена: ")
 
-#     def output_data(self):
-#         print("Модель:", self.model)
-#         print("Год:", self.year)
-#         print("Производитель:", self.manufacturer)
-#         print("Обьем двигателя:", self.engine_volume)
-#         print("Цвет:", self.color)
-#         print("Цена:", self.price)
+    def output_data(self):
+        print("Модель:", self.model)
+        print("Год:", self.year)
+        print("Производитель:", self.manufacturer)
+        print("Обьем двигателя:", self.engine_volume)
+        print("Цвет:", self.color)
+        print("Цена:", self.price)
 
-#     def get_model(self):
-#         return self.model
+    def get_model(self):
+        return self.model
 
-#     def get_year(self):
-#         return self.year
+    def get_year(self):
+        return self.year
 
-#     def get_manufacturer(self):
-#         return self.manufacturer
+    def get_manufacturer(self):
+        return self.manufacturer
 
-#     def get_engine_volume(self):
-#         return self.engine_volume
+    def get_engine_volume(self):
+        return self.engine_volume
 
-#     def get_color(self):
-#         return self.color
+    def get_color(self):
+        return self.color
 
-#     def get_price(self):
-#         return self.price
+    def get_price(self):
+        return self.price
 
 
-# car1 = Car("", "", "", "", "", "")
-# car1.input_data()
-# car1.output_data()
+car1 = Car("", "", "", "", "", "")
+car1.input_data()
+car1.output_data()
 
 
 '''Задание №2'''
@@ -59,53 +59,53 @@
 выпуска, издателя, жанр, автора, цену. Реализуйте методы класса для ввода данных, вывода 
 данных, реализуйте доступ к отдельным полям через методы класса.
 '''
-# class Book:
-#     def __init__(self, title, year, publisher, genre, author, price):
-#         self.title = title
-#         self.year = year
-#         self.publisher = publisher
-#         self.genre = genre
-#         self.author = author
-#         self.price = price
+class Book:
+    def __init__(self, title, year, publisher, genre, author, price):
+        self.title = title
+        self.year = year
+        self.publisher = publisher
+        self.genre = genre
+        self.author = author
+        self.price = price
 
-#     def input_data(self):
-#         self.title = input("Название: ")
-#         self.year = input("Год: ")
-#         self.publisher = input("Издатель: ")
-#         self.genre = input("Жанр: ")
-#         self.author = input("Автор: ")
-#         self.price = input("Цена: ")
+    def input_data(self):
+        self.title = input("Название: ")
+        self.year = input("Год: ")
+        self.publisher = input("Издатель: ")
+        self.genre = input("Жанр: ")
+        self.author = input("Автор: ")
+        self.price = input("Цена: ")
 
-#     def output_data(self):
-#         print("Название:", self.title)
-#         print("Год:", self.year)
-#         print("Издатель:", self.publisher)
-#         print("Жанр:", self.genre)
-#         print("Автор:", self.author)
-#         print("Цена:", self.price)
+    def output_data(self):
+        print("Название:", self.title)
+        print("Год:", self.year)
+        print("Издатель:", self.publisher)
+        print("Жанр:", self.genre)
+        print("Автор:", self.author)
+        print("Цена:", self.price)
 
-#     def get_title(self):
-#         return self.title
+    def get_title(self):
+        return self.title
 
-#     def get_year(self):
-#         return self.year
+    def get_year(self):
+        return self.year
 
-#     def get_publisher(self):
-#         return self.publisher
+    def get_publisher(self):
+        return self.publisher
 
-#     def get_genre(self):
-#         return self.genre
+    def get_genre(self):
+        return self.genre
 
-#     def get_author(self):
-#         return self.author
+    def get_author(self):
+        return self.author
 
-#     def get_price(self):
-#         return self.price
+    def get_price(self):
+        return self.price
 
 
-# book1 = Book("", "", "", "", "", "")
-# book1.input_data()
-# book1.output_data()
+book1 = Book("", "", "", "", "", "")
+book1.input_data()
+book1.output_data()
 
 
 '''Задание №3'''
@@ -114,47 +114,47 @@
 дату открытия, страну, город, вместимость. Реализуйте методы класса для ввода данных, 
 вывода данных, реализуйте доступ к отдельным полям через методы класса.
 '''
-# class Stadium:
-#     def __init__(self, name, opening_date, country, city, capacity):
-#         self.name = name
-#         self.opening_date = opening_date
-#         self.country = country
-#         self.city = city
-#         self.capacity = capacity
+class Stadium:
+    def __init__(self, name, opening_date, country, city, capacity):
+        self.name = name
+        self.opening_date = opening_date
+        self.country = country
+        self.city = city
+        self.capacity = capacity
 
-#     def input_data(self):
-#         self.name = input("Название стадиона: ")
-#         self.opening_date = input("Дата открытия: ")
-#         self.country = input("Страна: ")
-#         self.city = input("Город: ")
-#         self.capacity = input("Емкость: ")
+    def input_data(self):
+        self.name = input("Название стадиона: ")
+        self.opening_date = input("Дата открытия: ")
+        self.country = input("Страна: ")
+        self.city = input("Город: ")
+        self.capacity = input("Емкость: ")
 
-#     def output_data(self):
-#         print("Название стадиона:", self.name)
-#         print("Дата открытия:", self.opening_date)
-#         print("Страна:", self.country)
-#         print("Город:", self.city)
-#         print("Емкость:", self.capacity)
+    def output_data(self):
+        print("Название стадиона:", self.name)
+        print("Дата открытия:", self.opening_date)
+        print("Страна:", self.country)
+        print("Город:", self.city)
+        print("Емкость:", self.capacity)
 
-#     def get_name(self):
-#         return self.name
+    def get_name(self):
+        return self.name
 
-#     def get_opening_date(self):
-#         return self.opening_date
+    def get_opening_date(self):
+        return self.opening_date
 
-#     def get_country(self):
-#         return self.country
+    def get_country(self):
+        return self.country
 
-#     def get_city(self):
-#         return self.city
+    def get_city(self):
+        return self.city
 
-#     def get_capacity(self):
-#         return self.capacity
+    def get_capacity(self):
+        return self.capacity
 
 
-# stadium1 = Stadium("", "", "", "", "")
-# stadium1.input_data()
-# stadium1.output_data()
+stadium1 = Stadium("", "", "", "", "")
+stadium1.input_data()
+stadium1.output_data()
 
 
 '''ООП. Инкапсуляция'''
@@ -165,29 +165,29 @@
 конструктору. На основе главного класса создается класса потомок. В классе-потомке нужно 
 добавить числовое поле. У конструктора класса-потомка два аргумента.
 '''
-# class ParentClass:
-#     def __init__(self, text):
-#         self.text = text
+class ParentClass:
+    def __init__(self, text):
+        self.text = text
 
-#     def set_text(self, text):
-#         self.text = text
+    def set_text(self, text):
+        self.text = text
 
-#     def print_text(self):
-#         print("Text:", self.text)
-
-
-# class ChildClass(ParentClass):
-#     def __init__(self, text, number):
-#         super().__init__(text)
-#         self.number = number
-
-#     def set_number(self, number):
-#         self.number = number
-
-#     def print_number(self):
-#         print("Number:", self.number)
+    def print_text(self):
+        print("Text:", self.text)
 
 
-# child_obj = ChildClass("Hello", 10)
-# child_obj.print_text()
-# child_obj.print_number()
+class ChildClass(ParentClass):
+    def __init__(self, text, number):
+        super().__init__(text)
+        self.number = number
+
+    def set_number(self, number):
+        self.number = number
+
+    def print_number(self):
+        print("Number:", self.number)
+
+
+child_obj = ChildClass("Hello", 10)
+child_obj.print_text()
+child_obj.print_number()
--- hw16/main.py
+++ hw16/main.py
@@ -8,40 +8,40 @@
 '''
 
 
-# class Device:
-#     def __init__(self, brand, model):
-#         self.brand = brand
-#         self.model = model
+class Device:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
 
-#     def get_info(self):
-#         return f"Бренд: {self.brand}, Модель: {self.model}"
-
-
-# class CoffeeMachine(Device):
-#     def __init__(self, brand, model, capacity):
-#         super().__init__(brand, model)
-#         self.capacity = capacity
-
-#     def brew_coffee(self):
-#         return "Заваривание кофе..."
+    def get_info(self):
+        return f"Бренд: {self.brand}, Модель: {self.model}"
 
 
-# class Blender(Device):
-#     def __init__(self, brand, model, speed):
-#         super().__init__(brand, model)
-#         self.speed = speed
+class CoffeeMachine(Device):
+    def __init__(self, brand, model, capacity):
+        super().__init__(brand, model)
+        self.capacity = capacity
 
-#     def blend(self):
-#         return "Смешивание..."
+    def brew_coffee(self):
+        return "Заваривание кофе..."
 
 
-# class MeatGrinder(Device):
-#     def __init__(self, brand, model, power):
-#         super().__init__(brand, model)
-#         self.power = power
+class Blender(Device):
+    def __init__(self, brand, model, speed):
+        super().__init__(brand, model)
+        self.speed = speed
 
-#     def grind_meat(self):
-#         return "Измельчение мяса..."
+    def blend(self):
+        return "Смешивание..."
+
+
+class MeatGrinder(Device):
+    def __init__(self, brand, model, power):
+        super().__init__(brand, model)
+        self.power = power
+
+    def grind_meat(self):
+        return "Измельчение мяса..."
 
 
 '''
@@ -53,40 +53,40 @@
 '''
 
 
-# class Ship:
-#     def __init__(self, name, year):
-#         self.name = name
-#         self.year = year
+class Ship:
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
 
-#     def get_info(self):
-#         return f"Имя: {self.name}, Год: {self.year}"
-
-
-# class Frigate(Ship):
-#     def __init__(self, name, year, weapons):
-#         super().__init__(name, year)
-#         self.weapons = weapons
-
-#     def fire_weapon(self):
-#         return "Огнестрельное оружие..."
+    def get_info(self):
+        return f"Имя: {self.name}, Год: {self.year}"
 
 
-# class Destroyer(Ship):
-#     def __init__(self, name, year, missiles):
-#         super().__init__(name, year)
-#         self.missiles = missiles
+class Frigate(Ship):
+    def __init__(self, name, year, weapons):
+        super().__init__(name, year)
+        self.weapons = weapons
 
-#     def launch_missile(self):
-#         return "Запуск ракеты..."
+    def fire_weapon(self):
+        return "Огнестрельное оружие..."
 
 
-# class Cruiser(Ship):
-#     def __init__(self, name, year, size):
-#         super().__init__(name, year)
-#         self.size = size
+class Destroyer(Ship):
+    def __init__(self, name, year, missiles):
+        super().__init__(name, year)
+        self.missiles = missiles
 
-#     def maneuver(self):
-#         return "Выполнение маневра..."
+    def launch_missile(self):
+        return "Запуск ракеты..."
+
+
+class Cruiser(Ship):
+    def __init__(self, name, year, size):
+        super().__init__(name, year)
+        self.size = size
+
+    def maneuver(self):
+        return "Выполнение маневра..."
 
 
 '''
@@ -96,25 +96,25 @@
 '''
 
 
-# class Square:
-#     def __init__(self, x, y, side):
-#         self.x = x
-#         self.y = y
-#         self.side = side
+class Square:
+    def __init__(self, x, y, side):
+        self.x = x
+        self.y = y
+        self.side = side
 
-#     def area(self):
-#         return self.side ** 2
+    def area(self):
+        return self.side ** 2
 
 
-# class CircleInSquare(Square):
-#     def __init__(self, x, y, side):
-#         super().__init__(x, y, side)
+class CircleInSquare(Square):
+    def __init__(self, x, y, side):
+        super().__init__(x, y, side)
 
-#     def radius(self):
-#         return self.side / 2
+    def radius(self):
+        return self.side / 2
 
-#     def area(self):
-#         return 3.14 * (self.side / 2) ** 2
+    def area(self):
+        return 3.14 * (self.side / 2) ** 2
 
 
 '''
@@ -124,35 +124,35 @@
 '''
 
 
-# class Wheels:
-#     def __init__(self, size):
-#         self.size = size
+class Wheels:
+    def __init__(self, size):
+        self.size = size
 
-#     def rotate(self):
-#         return "Колеса вращающиеся..."
-
-
-# class Engine:
-#     def __init__(self, power):
-#         self.power = power
-
-#     def start(self):
-#         return "Запуск двигателя..."
+    def rotate(self):
+        return "Колеса вращающиеся..."
 
 
-# class Doors:
-#     def __init__(self, number):
-#         self.number = number
+class Engine:
+    def __init__(self, power):
+        self.power = power
 
-#     def open(self):
-#         return "Двери открываются..."
+    def start(self):
+        return "Запуск двигателя..."
 
 
-# class Car(Wheels, Engine, Doors):
-#     def __init__(self, size, power, number):
-#         Wheels.__init__(self, size)
-#         Engine.__init__(self, power)
-#         Doors.__init__(self, number)
+class Doors:
+    def __init__(self, number):
+        self.number = number
+
+    def open(self):
+        return "Двери открываются..."
+
+
+class Car(Wheels, Engine, Doors):
+    def __init__(self, size, power, number):
+        Wheels.__init__(self, size)
+        Engine.__init__(self, power)
+        Doors.__init__(self, number)
 
 
 '''
@@ -176,74 +176,74 @@ Ellipse — эллипс с заданными координатами верх
 '''
 
 
-# class Shape:
-#     def __init__(self, x, y):
-#         self.x = x
-#         self.y = y
+class Shape:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-#     def show(self):
-#         pass
+    def show(self):
+        pass
 
-#     def save(self):
-#         pass
+    def save(self):
+        pass
 
-#     def load(self):
-#         pass
-
-
-# class Square(Shape):
-#     def __init__(self, x, y, side):
-#         super().__init__(x, y)
-#         self.side = side
-
-#     def show(self):
-#         print(f"Квадрат: x={self.x}, y={self.y}, сторона={self.side}")
-
-#     def save(self):
-#         with open("square.txt", "w") as file:
-#             file.write(f"{self.x},{self.y},{self.side}")
-
-#     def load(self):
-#         with open("square.txt", "r") as file:
-#             data = file.readline().split(",")
-#             self.x = int(data[0])
-#             self.y = int(data[1])
-#             self.side = int(data[2])
+    def load(self):
+        pass
 
 
-# class Circle(Shape):
-#     def __init__(self, x, y, radius):
-#         super().__init__(x, y)
-#         self.radius = radius
+class Square(Shape):
+    def __init__(self, x, y, side):
+        super().__init__(x, y)
+        self.side = side
 
-#     def show(self):
-#         print(f"Круг: x={self.x}, y={self.y}, радиус={self.radius}")
+    def show(self):
+        print(f"Квадрат: x={self.x}, y={self.y}, сторона={self.side}")
 
-#     def save(self):
-#         with open("circle.txt", "w") as file:
-#             file.write(f"{self.x},{self.y},{self.radius}")
+    def save(self):
+        with open("square.txt", "w") as file:
+            file.write(f"{self.x},{self.y},{self.side}")
 
-#     def load(self):
-#         with open("circle.txt", "r") as file:
-#             data = file.readline().split(",")
-#             self.x = int(data[0])
-#             self.y = int(data[1])
-#             self.radius = int(data[2])
+    def load(self):
+        with open("square.txt", "r") as file:
+            data = file.readline().split(",")
+            self.x = int(data[0])
+            self.y = int(data[1])
+            self.side = int(data[2])
 
 
-# shapes = [Square(0, 0, 5), Circle(2, 3, 7)]
-# for shape in shapes:
-#     shape.show()
-#     shape.save()
+class Circle(Shape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y)
+        self.radius = radius
 
-# new_shapes = []
-# for shape in shapes:
-#     if isinstance(shape, Square):
-#         new_shape = Square(0, 0, 0)
-#     elif isinstance(shape, Circle):
-#         new_shape = Circle(0, 0, 0)
-#     new_shape.load()
-#     new_shapes.append(new_shape)
+    def show(self):
+        print(f"Круг: x={self.x}, y={self.y}, радиус={self.radius}")
 
-# for shape in new_shapes:
-#     shape.show()
+    def save(self):
+        with open("circle.txt", "w") as file:
+            file.write(f"{self.x},{self.y},{self.radius}")
+
+    def load(self):
+        with open("circle.txt", "r") as file:
+            data = file.readline().split(",")
+            self.x = int(data[0])
+            self.y = int(data[1])
+            self.radius = int(data[2])
+
+
+shapes = [Square(0, 0, 5), Circle(2, 3, 7)]
+for shape in shapes:
+    shape.show()
+    shape.save()
+
+new_shapes = []
+for shape in shapes:
+    if isinstance(shape, Square):
+        new_shape = Square(0, 0, 0)
+    elif isinstance(shape, Circle):
+        new_shape = Circle(0, 0, 0)
+    new_shape.load()
+    new_shapes.append(new_shape)
+
+for shape in new_shapes:
+    shape.show()
